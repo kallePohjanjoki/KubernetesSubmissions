5c25d3ef7edbd9ca1ae5b8a5a913e388b5ee80e6
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2026-07-12 15:16:29 +0300

    complete exercise 1.5

--- todo_app/README.md
+++ todo_app/README.md
@@ -1,3 +1,3 @@
 ## Todo app
 
-Deploy with `kubectl apply -f manifests/deployment.yaml  `
+Deploy with `kubectl apply -f manifests/deployment.yaml` and `kubectl port-forward todoapp-dep-54bc4bd6d5-pznxz 8000:8000`
--- todo_app/main.go
+++ todo_app/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = "NNNN"
-	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		http.ServeFile(w, r, "index.html")
+	})
 
 	fmt.Printf("Server started in port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
